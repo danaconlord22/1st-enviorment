3c8722812cff6a529e20be2c98a12ccae4673ab9
danaconlord22 <60923@gymnasium.ax>
2024-10-08 10:01:01 +0300

    Eget arbete Steg 4
    leif var sjuk så vi jobbade på steg 4
    
    alicia behövde hjälpa mig med ammend steget men annars så är hela steg 4 klart

--- firstsite/polls/urls.py
+++ firstsite/polls/urls.py
@@ -1,10 +1,13 @@
+
 from django.urls import path
+
 from . import views
 
-app_name = 'polls'
+
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/resultat", views.results, name="results"),
-    path("<int:question_id>/rosta", views.vote, name="vote"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/resultat/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/rosta/", views.vote, name="vote"),
 ]
