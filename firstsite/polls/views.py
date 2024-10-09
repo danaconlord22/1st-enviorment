e0b19ac342246464b28f333aa5ec116a584fb3f4
danaconlord22 <60923@gymnasium.ax>
2024-10-09 12:44:08 +0300

    steg 5 klart
    steg 5 är helt klart med hjälp av klassmedlemmar

--- firstsite/polls/views.py
+++ firstsite/polls/views.py
@@ -8,6 +8,17 @@ from django.views import generic
 
 from .models import Choice, Question
 
+from django.utils import timezone
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
@@ -21,6 +32,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
